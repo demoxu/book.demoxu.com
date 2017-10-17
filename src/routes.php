ee5a259470b75c7a610b369563547da6e0b89ef0
zhicai <zhicai@eastmoney.com>
2017-10-17 14:48:08 +0800

    add files

--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,47 @@
 <?php
-
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+$app->get('/', function ($request, $response, $args) {
+	$response->write("Hello, Welcome to Slim Framework!!!");
+	return $response;
 });
+
+// $app->get ( '/[{name}]', function (Request $request, Response $response, array $args) {
+// 	// Sample log message
+// 	$this->logger->info ( "Slim-Skeleton '/' route" );
+	
+// 	// Render index view
+// 	return $this->renderer->render ( $response, 'index.phtml', $args );
+// } );
+
+$app->get ( '/books/{id}', function ($request, $response, $args) {
+	// Show book identified by $args['id']
+} );
+
+$app->post ( '/books', function ($request, $response, $args) {
+	// Create new book
+} );
+
+$app->put ( '/books/{id}', function ($request, $response, $args) {
+	// Update book identified by $args['id']
+} );
+
+$app->delete ( '/books/{id}', function ($request, $response, $args) {
+	// Delete book identified by $args['id']
+} );
+
+// $app->map ( [ 
+// 		'GET',
+// 		'POST' 
+// ], '/books', function ($request, $response, $args) {
+// 	// Create new book or list all books
+// } );
+
+// $app->get ( '/home', '\HomeController:home' );
+
+// $app->get ( '/method1', '\MyController:method1' );
+// $app->get ( '/method2', '\MyController:method2' );
+// $app->get ( '/method3', '\MyController:method3' );
--- src/settings.php
+++ src/settings.php
@@ -1,19 +1,29 @@
 <?php
-return [
-    'settings' => [
-        'displayErrorDetails' => true, // set to false in production
-        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
-        // Renderer settings
-        'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
-        ],
-
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
-    ],
+return [ 
+		'settings' => [ 
+				'displayErrorDetails' => true, // set to false in production
+				'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+				'determineRouteBeforeAppMiddleware' => false,
+				'db' => [ 
+						'driver' => 'mysql',
+						'host' => 'localhost',
+						'database' => 'book',
+						'username' => 'root',
+						'password' => 'password',
+						'charset' => 'utf8',
+						'collation' => 'utf8_unicode_ci',
+						'prefix' => '' 
+				],
+				// Renderer settings
+				'renderer' => [ 
+						'template_path' => __DIR__ . '/../templates/' 
+				],
+				
+				// Monolog settings
+				'logger' => [ 
+						'name' => 'slim-app',
+						'path' => isset ( $_ENV ['docker'] ) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+						'level' => \Monolog\Logger::DEBUG 
+				] 
+		] 
 ];
